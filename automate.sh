b364141e1ca337ca7bb43e882d07612dbda02ab8
MaxMIs <27479366+MaxMIs@users.noreply.github.com>
2024-11-13 13:41:31 +0000

    upd

--- automate.sh
+++ automate.sh
@@ -15,7 +15,7 @@ git clean -df
 git pull
 
 # Step 5: Install dependencies and build
-npm install
+npm i
 npm run build
 
 # Step 6: Go back to the parent directory
@@ -27,8 +27,10 @@ pm2 delete ecosystem.config.js
 # Step 8: Remove files from the parent folder except .env and .temp-ci/
 find . -maxdepth 1 ! -name '.env' ! -name '.temp-ci' -exec rm -rf {} +
 
+mv .temp-ci/* .
+
+rm -d .temp-ci
+
 # Step 9: Start the pm2 process again
 pm2 start ecosystem.config.js
 
-# Step 10: Move the .git folder back to the parent directory (Optional)
-mv .temp-ci/.git .
